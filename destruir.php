f06b207765b29610a46e606be1fea6039cbaa67f
teamhackevin <kevinexequielaguirrejimenez@gmail.com>
2018-11-29 20:31:14 -0600

    conexion a base de datos y muestra de datos

--- destruir.php
+++ destruir.php
@@ -1,5 +1,6 @@
 
 <?php
+/*
 session_start();
 
 if(isset($_SESSION['nombre_usuario']))
@@ -7,11 +8,17 @@ if(isset($_SESSION['nombre_usuario']))
 	unset($_SESSION['nombre_usuario']);
 	session_destroy($_SESSION['nombre_usuario']);
 	
-	header('location:hotel.php');	
+	header('location:login.php');	
 }
 else
 {
 	echo"no se cerro la sesion";
 	
-}
+}*/
+
+session_start();
+session_destroy();
+print "<script>window.location='../login.php';</script>"
+
+
 ?>
